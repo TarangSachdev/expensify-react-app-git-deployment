d1fe3804f32f7762458785746c09bd2f7e4c9374
Tarang Sachdev <tarang.smartsense@gmail.com>
2018-02-26 13:11:38 +0530

    Intial Commit

---
+++ src/playground/spread-operator-objects.js
@@ -0,0 +1,14 @@
+
+// spread operator using object spread operator
+const user = {
+    name: 'tarang',
+    age: 14
+}
+
+console.log({
+    // age: 24,
+    ...user,
+    lname: 'sachdev',
+    location: 'Ahmedabad',
+    age: 24
+})
