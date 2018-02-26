95b1f54a980f13c1bffd61453a9416f2beb89208
Tarang Sachdev <tarang.smartsense@gmail.com>
2018-02-26 14:42:24 +0530

    remove dummy expenses data

--- src/app.js
+++ src/app.js
@@ -13,20 +13,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 1200, createdAt: 100 }));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 120, amount: 1200, }));
-store.dispatch(addExpense({ description: 'Rent', amount: 12000, createdAt: 110 }));
-store.dispatch(addExpense({ description: 'Cabel bill', amount: 1400 }));
-
-
-// store.dispatch(editExpense(expenseTwo.expense.id, { amount: 45 }));
-
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
